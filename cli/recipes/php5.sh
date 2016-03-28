fb6ca76cb68b83a10711770e1727b6b81e83eae2
Thiago Senna <thiago.senna@4linux.com.br>
2016-03-28 11:15:12 -0300

    minor enhancements

--- cli/recipes/php5.sh
+++ cli/recipes/php5.sh
@@ -8,8 +8,14 @@ apt-get -y install nodejs
 apt-get -y install nodejs-legacy
 apt-get -y install npm
 
+###########################################################
+# FrontEnd Stuff
+###########################################################
+
 npm install -g bower
 npm install -g gulp grunt-cli
+npm install -g less
+gem install sass
 
 ###########################################################
 # Apache2 Under Vagrant
